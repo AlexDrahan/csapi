f8250d3ddc418577be7baa1dd95436ab63ac4c36
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-04-11 12:49:21 +0300

    Add trial connection database MySQL

---
+++ src/main/java/com/example/csapi/Database.java
@@ -0,0 +1,30 @@
+package com.example.csapi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        /*String url = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "";
+        String password = "";*/
+
+
+       /* try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }*/
+    }
+}
